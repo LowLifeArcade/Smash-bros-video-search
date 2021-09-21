3c0c9b98f6fb4c1f8d7a1fd67ace5abdc3b02ed6
Sonny Brown <68655112+LowLifeArcade@users.noreply.github.com>
2021-09-20 20:18:06 -0700

    Made this update from github.dev

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './components/App'
-
-ReactDOM.render(
-  <App />,
-  document.querySelector('#root')
-)
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+// test
+ReactDOM.render(<App />, document.querySelector('#root'));
